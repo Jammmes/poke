e6ad82f766f30d6ac7efac26c32f77529e33a5ee
Evgenii Vershinin <evgenii_vershinin@epam.com>
2019-11-19 12:47:49 +0400

    modify footer, add pagination, fix content

--- src/components/app-footer/app-footer.tsx
+++ src/components/app-footer/app-footer.tsx
@@ -1,7 +1,29 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useCallback } from 'react';
+import { Pagination } from 'antd';
+import 'antd/lib/pagination/style/css';
 
-import styles from './app-footer.scss'
+import styles from './app-footer.scss';
+import { Panel } from '../panel';
 
 export const AppFooter: FunctionComponent<{}> = () => {
-  return <div className={styles.root}> pokemon list 2019 </div>
-}
+
+  const handleOnShowSizeChange = useCallback(() => {
+    console.log('change');
+  }, []);
+
+  return (
+  <footer className={styles.root}>
+    <div className={styles.header}>
+    <Panel>
+      <Pagination
+        showSizeChanger
+        onShowSizeChange={handleOnShowSizeChange}
+        defaultCurrent={3}
+        total={500}
+      />
+    </Panel>
+    </div>
+    <div className={styles.footer}>pokemon list 2019</div>
+   </footer>
+  );
+};
