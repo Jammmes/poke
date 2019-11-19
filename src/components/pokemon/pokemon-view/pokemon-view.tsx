2319fd2a9ebe8f7d923e2e5cd32072ca57ab406f
Eugenii <glazov.it@gmail.com>
2019-11-19 23:00:35 +0400

    improve UI pokemon-view

--- src/components/pokemon/pokemon-view/pokemon-view.tsx
+++ src/components/pokemon/pokemon-view/pokemon-view.tsx
@@ -1,12 +1,12 @@
 import React, { FunctionComponent } from 'react';
-import { Card, Tag, Statistic  } from 'antd';
-import 'antd/lib/card/style/css';
-import 'antd/lib/tag/style/css';
-import 'antd/lib/statistic/style/css';
+import { Typography } from 'antd';
+import 'antd/lib/typography/style/css';
+const { Text } = Typography;
 
 import styles from './pokemon-view.scss';
 import { PokemonType } from '@/api/interfaces';
 import { Panel, Header, Content, Footer } from '@/components/panel';
+import { Tags } from '@/components/tags';
 
 export interface IPokemonViewProps {
   id: number;
@@ -29,23 +29,28 @@ export const PokemonView: FunctionComponent<IPokemonViewProps> = (
   },
 ) => {
   return (
-      <Panel className={styles.root}>
-        <Header className={styles.title}>
-          {name}
-        </Header>
-        <Content className={styles.content}>
-        <img  className={styles.image} src={image} alt='...'/>
-          <div className={styles.info}>
-          <Statistic title='Expirience' value ={base_experience} />
-          <Statistic title='height' value ={height} />
-          <Statistic title='weight' value ={weight} />
-          </div>
-        </Content>
-        <Footer>
-        <div className ={styles.tags}>
-            {types.map(tag => <Tag key={tag}>{tag}</Tag>)}
-          </div>
-        </Footer>
-      </Panel>
+    <Panel className={styles.root}>
+      <Header>
+        <div title={name} className={styles.title}>{name}</div>
+      </Header>
+      <Content className={styles.content}>
+        <img className={styles.image} src={image} alt='...' />
+        <div className={styles.info}>
+          <Text strong>Expirience: </Text>
+          <Text code>{base_experience}</Text>
+        </div>
+        <div className={styles.info}>
+          <Text strong>Height: </Text>
+          <Text code>{height}</Text>
+        </div>
+        <div className={styles.info}>
+          <Text strong>Weight: </Text>
+          <Text code>{weight}</Text>
+        </div>
+      </Content>
+      <Footer>
+        <Tags tags={types} />
+      </Footer>
+    </Panel>
   );
 };
