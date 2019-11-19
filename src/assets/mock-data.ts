f078dab3702811efb0c8b1e9b26270bbe264935b
Evgenii Vershinin <evgenii_vershinin@epam.com>
2019-11-19 12:46:02 +0400

    add mock pokemon data

---
+++ src/assets/mock-data.ts
@@ -0,0 +1,60 @@
+
+export const mockPokemons =
+  {
+    pokemons: [
+      {
+        id: 1,
+        name: 'pickachu',
+        base_experience: 29,
+        height: 50,
+        weight: 40,
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/57.png',
+        types: ['normal', 'fighting'],
+      },
+      {
+        id: 2,
+        name: 'iceman',
+        base_experience: 30,
+        height: 150,
+        weight: 70,
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/21.png',
+        types: ['ice'],
+      },
+      {
+        id: 3,
+        name: 'dragon',
+        base_experience: 59,
+        height: 250,
+        weight: 65,
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png',
+        types: ['grass', 'dragon'],
+      },
+      {
+        id: 4,
+        name: 'man',
+        base_experience: 49,
+        height: 10,
+        weight: 44,
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/22.png',
+        types: ['normal', 'fighting'],
+      },
+      {
+        id: 5,
+        name: 'sito',
+        base_experience: 39,
+        height: 15,
+        weight: 89,
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/37.png',
+        types: ['fire', 'fighting'],
+      },
+      {
+        id: 6,
+        name: 'aqua',
+        base_experience: 19,
+        height: 22,
+        weight: 65,
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/17.png',
+        types: ['water', 'fighting'],
+      },
+    ],
+  };
