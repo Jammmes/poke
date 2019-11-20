95ffa05812269df86f0517df8cb45f1b9f660936
Eugenii <glazov.it@gmail.com>
2019-11-20 23:44:48 +0400

    Add mobx-logger

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'mobx-react';
+import { enableLogging } from 'mobx-logger';
 
 import App from './app/app';
 
 import { SearchFilterStore } from '@/stores/searchFilterstore';
+import { PokemonStore } from '@/stores/pokemonStore';
+
+const config = {
+  predicate: () => true,
+  action: true,
+  reaction: true,
+  transaction: true,
+  compute: true
+};
+
+enableLogging(config);
+
 const stores = {
   SearchFilterStore,
+  PokemonStore,
 };
 
 render(
-<Provider {...stores}>
-  <App />
-</Provider>
-, document.getElementById('reactContainer'));
+  <Provider {...stores}>
+    <App />
+  </Provider>
+  , document.getElementById('reactContainer'));
